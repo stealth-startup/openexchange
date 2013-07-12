b7ac47cdc536b7b97fc33ef46d7cc38baa79b805
Rex <fdrex1987@gmail.com>
2013-07-12 15:07:25 +0800

    new process_block script using python instead of bash

--- DjangoWebServer/process_block.py
+++ DjangoWebServer/process_block.py
@@ -1,4 +1,17 @@
+#!/usr/bin/python
+
 __author__ = 'Rex'
 
+import os
 
-#TODO implement this
+
+def start():
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(SCRIPT_DIR)
+    while True:
+        if os.system('python manage.py processblock') == 2:
+            break
+
+
+if __name__ == "__main__":
+    start()
